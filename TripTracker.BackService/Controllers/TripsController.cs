b6811e45cd9a9d242058e36b8904b3ec8e84a1cd
Shadowrise <zellion@yandex.ru>
2019-02-11 23:01:50 +0300

    With working Entity Framework

--- TripTracker.BackService/Controllers/TripsController.cs
+++ TripTracker.BackService/Controllers/TripsController.cs
@@ -1,5 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TripTracker.BackService.Data;
 using TripTracker.BackService.Models;
 
 namespace TripTracker.BackService.Controllers
@@ -7,46 +10,79 @@ namespace TripTracker.BackService.Controllers
     [Route("api/[controller]")]
     public class TripsController : Controller
     {
-        public TripsController(Repository repository)
-        {
-            _repository = repository;
-        }
+        private TripContext _context;
 
-        private Repository _repository;
+        public TripsController(TripContext context)
+        {
+            _context = context;
+        }
 
         // GET api/Trips
         [HttpGet]
-        public IEnumerable<Trip> Get()
+        public async Task<IActionResult> Get()
         {
-            return _repository.Get();
+            var trips = await _context.Trips.AsNoTracking().ToListAsync();
+            return Ok(trips);
         }
 
         // GET api/Trips/5
         [HttpGet("{id}")]
         public Trip Get(int id)
         {
-            return _repository.Get(id);
+            return _context.Trips.Find(id);
         }
 
         // POST api/Trips
         [HttpPost]
-        public void Post([FromBody]Trip value)
+        public IActionResult Post([FromBody]Trip value)
         {
-            _repository.Update(value);
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Trips.Add(value);
+            _context.SaveChanges();
+
+            return Ok();
         }
 
         // PUT api/Trips/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody]Trip value)
+        public async Task<IActionResult> PutAsync(int id, [FromBody]Trip value)
         {
-            _repository.Update(value);
+
+            if (!_context.Trips.Any(t => t.Id == id))
+            {
+                return NotFound();
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Trips.Update(value);
+            await _context.SaveChangesAsync();
+
+            return Ok();
         }
 
-        // DELETE api/values/5
+        // DELETE api/Trips/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public IActionResult Delete(int id)
         {
-            _repository.Remove(id);
+            var myTrip = _context.Trips.Find(id);
+
+            if (myTrip == null)
+            {
+                return NotFound();
+            }
+
+            _context.Trips.Remove(myTrip);
+            _context.SaveChanges();
+
+            return NoContent();
         }
     }
 }
