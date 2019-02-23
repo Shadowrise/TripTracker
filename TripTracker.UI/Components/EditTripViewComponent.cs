21bd31f9bd24ca38979128afef978c48fb739fdd
Shadowrise <zellion@yandex.ru>
2019-02-23 23:28:04 +0300

    Completed module 5

---
+++ TripTracker.UI/Components/EditTripViewComponent.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TripTracker.BackService.Models;
+
+namespace TripTracker.UI.ViewComponents
+{
+    public class EditTripViewComponent: ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync(Trip trip)
+        {
+            await Task.Delay(0);
+            return View("EditTrip", trip);
+        }
+    }
+}
